873dfe05e2fc0928a358b5c6eb3ffe6c2f4d430c
boskoa <bosko.ap@gmail.com>
2024-05-03 12:59:21 +0200

    Car physics

---
+++ controls.js
@@ -0,0 +1,53 @@
+class Controls {
+  constructor() {
+    this.forward = false;
+    this.left = false;
+    this.right = false;
+    this.reverse = false;
+
+    this.#addKeyboardListeners();
+  }
+
+  #addKeyboardListeners() {
+    document.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "ArrowUp":
+          this.forward = true;
+          break;
+        case "ArrowDown":
+          this.reverse = true;
+          break;
+        case "ArrowLeft":
+          this.left = true;
+          break;
+        case "ArrowRight":
+          this.right = true;
+          break;
+        default:
+          return;
+      }
+      console.table(this);
+    });
+
+    document.addEventListener("keyup", (e) => {
+      switch (e.key) {
+        case "ArrowUp":
+          this.forward = false;
+          break;
+        case "ArrowDown":
+          this.reverse = false;
+          break;
+        case "ArrowLeft":
+          this.left = false;
+          break;
+        case "ArrowRight":
+          this.right = false;
+          break;
+        default:
+          return;
+      }
+
+      console.table(this);
+    });
+  }
+}
